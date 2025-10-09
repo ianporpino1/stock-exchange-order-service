7364af7434c2740383b4539f1869f70ff3a40a67
Ianporpino1 <ian.porpino.017@ufrn.edu.br>
2025-10-08 21:13:52 -0300

    last price

--- src/main/java/com/stockexchange/orderservice/OrderServiceApplication.java
+++ src/main/java/com/stockexchange/orderservice/OrderServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class OrderServiceApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
